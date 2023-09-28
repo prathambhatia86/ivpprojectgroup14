89f63db5966a5a2e5182d6971dd53dcc22dd939b
prathambhatia86 <85384335+prathambhatia86@users.noreply.github.com>
2023-09-28 23:02:50 +0530

    pushed data folder

---
+++ Data/download.sh
@@ -0,0 +1,11 @@
+wget http://images.cocodataset.org/zips/train2014.zip
+wget http://images.cocodataset.org/zips/val2014.zip
+wget http://images.cocodataset.org/zips/test2015.zip
+
+wget https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/Questions_Train_mscoco.zip
+wget https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/Questions_Val_mscoco.zip 
+wget https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/Questions_Test_mscoco.zip
+wget https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/Annotations_Train_mscoco.zip
+wget https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/Annotations_Val_mscoco.zip 
+wget http://visualqa.org/data/mscoco/vqa/v2_Questions_Val_mscoco.zip; 
+wget http://download.tensorflow.org/models/resnet_v2_152_2017_04_14.tar.gz
